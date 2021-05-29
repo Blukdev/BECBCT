decb1517ae462605b631b649213134ef69dc6128
东灯 <43312495+Lampese@users.noreply.github.com>
2021-05-29 21:57:04 +0800

    修复ArmorStandNameBuilder()的问题

--- becbct.h
+++ becbct.h
@@ -15,28 +15,31 @@ struct TrigonometricFunction{
     float omega;
 };
 string ArmorStandNameBuilder(int Num){
-    int High = Num/26;
-    int Low = Num%26;
+    int High=Num/26;
+    int Low=Num%26;
     string Name;
-    if(High!=0)Name = char(High+64)+char(Low+64);
-    else Name = char(Low+64);
+    if(High!=0){
+    	Name="";
+    	Name+=char(High+64);
+	Name+=char(Low+64);
+    }
+    else Name=char(Low+64);
     return Name;
 }
 void FourierSeriesBuilder(TrigonometricFunction TriFunc[],int n){
-    string name, namep, SelectorA, SelectorB, Head;
+    string name,namep,SelectorA,SelectorB,Head;
     ofstream ofs;
     ofs.open("in.txt");
     ofs<<n<<endl;
 	for(register int i=0;i<n;++i){
-	    name = ArmorStandNameBuilder(i);
-	    namep = ArmorStandNameBuilder(i+1);
-            SelectorA = "@e[type=armor_stand,name=" + name + "]";
-    	    SelectorB = "@e[type=armor_stand,name=" + namep + "]";
+	    name=ArmorStandNameBuilder(i);
+	    namep=ArmorStandNameBuilder(i+1);
+            SelectorA="@e[type=armor_stand,name="+name+"]";
+    	    SelectorB="@e[type=armor_stand,name="+namep+"]";
     	    i==0?Head="[rcb]":Head="[ccb]";
     	    ofs<<Head<<" execute "<<SelectorA<<" ~~~ tp @s ~~~ ~"<<roundt(TriFunc[i].omega)<<"~"<<endl;
     	    ofs<<"[ccb]"<<" execute "<<SelectorA<<" ~~~ tp "<<SelectorB<<" ^^^"<<roundt(TriFunc[i].alpha)<<endl;
 	}
     ofs.close();
-    //execute <Selector> ~~~ <FuncName> -----
 }
 #endif
