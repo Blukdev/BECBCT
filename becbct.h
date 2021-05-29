e85b7f52ddca34b9078819acb615aae73019cf5a
东灯 <43312495+Lampese@users.noreply.github.com>
2021-05-29 20:56:07 +0800

    del commandbuilder()

--- becbct.h
+++ becbct.h
@@ -77,36 +77,6 @@ void commandblock(string cmd){
 	Sleep(100);
 	press(27);
 }
-void commandbuilder(){
-    freopen("in.txt","r",stdin);
-	int x=1,y=5,z=1;
-	int types;
-	cin>>types;
-	string commands[types+5];
-	string commandblocks[types+5];
-	for(int i=1;i<=types;i++){
-		cin>>commandblocks[i];
-		getchar();
-		getline(cin,commands[i]);
-	}
-	Sleep(3000);
-	for(int i=1;i<=types;i++){
-		string type;
-		if(commandblocks[i]=="[rcb]")type="repeating_command_block";
-		else if(commandblocks[i]=="[ccb]")type="chain_command_block";
-		else type="command_block";
-		command("setblock "+to_string(x-i+1)+" 4 1 "+type+" 4");
-		Sleep(500);
-	}
-	Sleep(400);
-	for(int i=1;i<=types;i++){
-		command("tp @s "+to_string(x-i+1)+" 5 1 -90 90");
-		Sleep(500);
-		commandblock(commands[i]);
-		Sleep(800);
-	}
-	fclose(stdin);
-}
 string ArmorStandNameBuilder(int Num){
     int High = Num/26;
     int Low = Num%26;
